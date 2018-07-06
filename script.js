37a13fce4385ff57cddafa429ff29758461ec8e2
Adrian Martinez <adrian.martinez@scholar.smashprogram.org>
2018-07-06 09:53:43 -0700

    first

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // Global Variables
 var p5Canvas;
 var myName;
+var count;
+var xvelocity;
 
 // initialize global variables in setup() function
 function setup() {
   p5Canvas = createCanvas(800, 600);
   p5Canvas.parent("#p5-canvas");
   myName = select("#my-name");
-  myName.html("[Your Name Here]");
+  myName.html("Adrian");
 }
 
 function draw() {
   background(100, 0, 100);
-}
+    ellipse( 145, 225, 230, 100);
+
+} 
