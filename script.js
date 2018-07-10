df44bd909effb666018918407428525a1ce93412
Adrian Martinez <adrian.martinez@scholar.smashprogram.org>
2018-07-09 22:01:06 -0700

    first

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 var p5Canvas;
 var myName;
 var count;
-var xvelocity;
+var x=40;
+var y=40;
+var a=40;
+var b=40;
+
 
 // initialize global variables in setup() function
 function setup() {
@@ -12,8 +16,27 @@ function setup() {
   myName.html("Adrian");
 }
 
-function draw() {
-  background(100, 0, 100);
-    ellipse( 145, 225, 230, 100);
-
-} 
+function draw(){
+    background(100, 0, 100);
+    draw10circles();
+}
+            
+function draw10circles(){
+    var count= 0;
+    while(count<=5){
+        fill("gold");
+        ellipse(x+50, y+50, a, b);
+        ellipse(x+100, y+100, a, b);
+        ellipse(x+150, y+150, a, b);
+        ellipse(x+200, y+200, a, b);
+        ellipse(x+250, y+250, a, b);
+        ellipse(x+300, y+300, a, b);
+        ellipse(x+350, y+350, a, b);
+        ellipse(x+400, y+400, a, b);
+        ellipse(x+450, y+450, a, b);
+        ellipse(x+500, y+500, a, b);
+        count+=1;
+        x+=.2;
+        y-=.2;
+    }
+}
